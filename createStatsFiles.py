0325512f10fc3e0cf88833aa43adb86990d5e846
Jeremy Foote <jdfoote1@gmail.com>
2014-02-15 20:44:35 -0500

    Changing to ratio measures

--- createStatsFiles.py
+++ createStatsFiles.py
@@ -13,6 +13,7 @@ endDate = config['endDate']
 delta = dt.timedelta(days=config['timeDelta']) # Time between networks, in days
 behavior = config['behavior']
 categories = config['editCats']
+non_ratio_cats = ['all_edits']
 otherStats = ['start_date','end_date','active_days','all_edits','user_id']
 
 print categories
@@ -34,15 +35,26 @@ with open(nodeList, 'rb') as i:
         attributeStats = {c:[] for c in categories}
         for date in dateList:
             stats = nT.getStats(user, date, categories, otherStats)
+            totEdits = float(sum([stats[x] for x in categories if x not in non_ratio_cats]))
+            totRatio = 0
             if not stats:
                 print "no stats for {}".format(user)
             for stat in stats:
                 if stat == behavior:
                     behaviorStats.append(stats[stat])
                 elif stat in categories:
-                    attributeStats[stat].append(stats[stat])
+                    if stat in non_ratio_cats:
+                        attributeStats[stat].append(stats[stat])
+                    else:
+                        if totEdits > 0:
+                            attributeStats[stat].append(stats[stat]/totEdits)
+                            totRatio += stats[stat]/totEdits
+                        else:
+                            attributeStats[stat].append(0)
                 else:
                     continue
+            if totEdits > 0:
+                assert abs(1-totRatio) < .01
         behavTotal.append(behaviorStats)
         for att in attributeStats:
             attributeTotal[att].append(attributeStats[att])
