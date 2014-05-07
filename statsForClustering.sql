af2de5d4d217a275cd740191e46ca93243c1218a
Jeremy Foote <jdfoote1@gmail.com>
2014-05-07 18:56:24 -0400

    Adding check for bot - reflects current data

--- statsForClustering.sql
+++ statsForClustering.sql
@@ -4,6 +4,7 @@
 		FROM userstats INNER JOIN users 
 		ON userstats.user_id = users.user_id 
 		WHERE userstats.end_date >= users.first_edit 
+		AND users.is_bot = FALSE
 		AND userstats.all_edits > 5) 
 TO '/home/jeremy/Desktop/userStats.csv' 
 DELIMITER ',' CSV HEADER;
