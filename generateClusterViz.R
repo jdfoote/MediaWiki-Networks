d05b65c447937dcd04555494fdf6bd100be7d259
Jeremy Foote <jdfoote1@gmail.com>
2014-05-13 16:31:25 -0400

    Adding line graphs. There appears to be a bug in the legend. Need to look into it

--- generateClusterViz.R
+++ generateClusterViz.R
@@ -12,6 +12,7 @@ clusterDF <- as.data.frame(read.csv(paste('clustersByID',trailingZeroes,'Trailin
 minMonths = 2
 
 makeGraph <- function(clusters, graphType="fill"){
+		ylabel <- if(graphType == 'fill') "Proportion of users in each cluster" else "Number of users in each cluster"
 		clus1 <- apply(clusters, 2, function(x) {sum(x=='1', na.rm=TRUE)})
 		clus2 <- apply(clusters, 2, function(x) {sum(x=='2', na.rm=TRUE)})
 		clus3 <- apply(clusters, 2, function(x) {sum(x=='3', na.rm=TRUE)})
@@ -27,6 +28,23 @@ makeGraph <- function(clusters, graphType="fill"){
 		p <- p + scale_x_discrete(breaks=NULL, name="Time since joining") + ylab("Proportion of users in each cluster")
 		return(p)
 }
+
+makeLineGraph <- function(clusters){
+		clus1 <- apply(clusters, 2, function(x) {sum(x=='1', na.rm=TRUE)})
+		clus2 <- apply(clusters, 2, function(x) {sum(x=='2', na.rm=TRUE)})
+		clus3 <- apply(clusters, 2, function(x) {sum(x=='3', na.rm=TRUE)})
+		clus0 <- apply(clusters, 2, function(x) {sum(x=='0', na.rm=TRUE)})
+		clusters2 <- data.frame(clus0, clus1, clus2, clus3)
+		c2 <- t(clusters2)
+		c3 <- as.data.frame(c2)
+		c3$id = c('Low Activity Cluster', 'Central Members', 'Peripheral Experts', 'Newbies')
+		c3 <- c3[order(c(1,4,3,2)),]
+		p <- ggplot(melt(c3, id.vars="id"), aes(x=variable, y=value, group=id, color = id)) + geom_path(alpha=0.5)
+		p <- p + scale_fill_manual(values=c("#69D2E7","#A7DBD8","#E0E4CC","#F38630"))
+		p <- p + scale_x_discrete(breaks=NULL, name="Time since joining") + ylab("Number of users in each cluster")
+		return(p)
+}
+
 #print(ggplot(clusters.mlt) +
  # stat_summary(aes(x=variable, y=value, fill=id, group=id), fun.y=sum, position="fill", geom="area"))
 
@@ -43,6 +61,7 @@ ggsave("../Results/Role2_2+.png", makeGraph(cl2))
 cl3 <- clusterDF[apply(clusterDF, 1, function(x) {sum(x[2:76] == "3", na.rm=TRUE) >= minMonths}),]
 ggsave("../Results/Role3_2+.png", makeGraph(cl3))
 
+
 # Calculate a version for each of the modal clusters
 Mode <- function(x) {
 		x <- x[!is.na(x)]
@@ -55,15 +74,19 @@ modeVals <- apply(clusterDF[,2:76], 1, function(x) Mode(x))
 mode0 <- clusterDF[modeVals == 0,]
 ggsave(paste("../Results/Role0Mode",trailingZeroes,"Trailing.png",sep=""), makeGraph(mode0))
 ggsave(paste("../Results/Role0Mode",trailingZeroes,"TrailingStack.png",sep=""), makeGraph(mode0,"stack"))
+ggsave(paste("../Results/Role0Mode",trailingZeroes,"TrailingLine.png",sep=""), makeLineGraph(mode0))
 
 mode1 <- clusterDF[modeVals == 1,]
 ggsave(paste("../Results/Role1Mode",trailingZeroes,"Trailing.png",sep=""), makeGraph(mode1))
 ggsave(paste("../Results/Role1Mode",trailingZeroes,"TrailingStack.png",sep=""), makeGraph(mode1,"stack"))
+ggsave(paste("../Results/Role1Mode",trailingZeroes,"TrailingLine.png",sep=""), makeLineGraph(mode0))
 
 mode2 <- clusterDF[modeVals == 2,]
 ggsave(paste("../Results/Role2Mode",trailingZeroes,"Trailing.png",sep=""), makeGraph(mode2))
 ggsave(paste("../Results/Role2Mode",trailingZeroes,"TrailingStack.png",sep=""), makeGraph(mode2,"stack"))
+ggsave(paste("../Results/Role2Mode",trailingZeroes,"TrailingLine.png",sep=""), makeLineGraph(mode0))
 
 mode3 <- clusterDF[modeVals == 3,]
 ggsave(paste("../Results/Role3Mode",trailingZeroes,"Trailing.png",sep=""), makeGraph(mode3))
 ggsave(paste("../Results/Role3Mode",trailingZeroes,"TrailingStack.png",sep=""), makeGraph(mode3,"stack"))
+ggsave(paste("../Results/Role3Mode",trailingZeroes,"TrailingLine.png",sep=""), makeLineGraph(mode0))
