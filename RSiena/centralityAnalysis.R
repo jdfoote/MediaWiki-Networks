0c0219169e0014af411991765004480cec82c7ee
Jeremy Foote <jdfoote1@gmail.com>
2014-06-02 21:44:33 -0400

    Version used for eigCentrality (good fit, not significant)

--- RSiena/centralityAnalysis.R
+++ RSiena/centralityAnalysis.R
@@ -1,4 +1,3 @@
-
 if (!"RSiena" %in% installed.packages()) install.packages("RSiena")
 if (!"sna" %in% installed.packages()) install.packages("sna")
 if (!"network" %in% installed.packages()) install.packages("network")
@@ -12,9 +11,9 @@ config <- yaml.load_file('~/Programming/WeRelate/Code/config.yaml')
 
 # Determines how to dichotomize. Only values greater than dichotCutoff will be included
 # in matrix
-dichotCutoff = 2.9
+dichotCutoff = 1.9
 # Index of first date to include
-firstTime = 1
+firstTime = 4
 # Index of last date to include
 lastTime = 8
 numWaves = 8
@@ -33,7 +32,7 @@ globCom <- array(sapply(filesToImport[seq(2,length(filesToImport),4)], importNet
 locCom <- array(sapply(filesToImport[seq(3,length(filesToImport),4)], importNetwork), c(nodeCount, nodeCount, numWaves)) 
 obs <- array(sapply(filesToImport[seq(4,length(filesToImport),4)], importNetwork), c(nodeCount, nodeCount, numWaves))
 allNets <- collab + globCom + locCom + obs
-allNets[allNets > 1] <- 1
+allNets[allNets > 0] <- 1
 
 Attributes <- as.data.frame(read.csv('../RSienaAttributeFile2.csv', header = TRUE))
 # Sort by date, then userID
@@ -70,7 +69,7 @@ daysSinceJoining <- makeVarCovar(dsj)
 
 # Calculate centrality, and add it as a covariate.
 # MAKE SURE TO CHANGE THE NETWORK THIS APPLIES TO
-eigenvectorCentrality <- makeVarCovar(apply(obs,MARGIN=3,FUN=evcent))
+eigenvectorCentrality <- makeVarCovar(apply(allNets,MARGIN=3,FUN=evcent))
 
 # Create dependent variables
 observation <- sienaNet(obs[,,firstTime:lastTime])
@@ -79,42 +78,42 @@ globalCom <- sienaNet(globCom[,,firstTime:lastTime])
 collaboration <- sienaNet(collab[,,firstTime:lastTime])
 allInteractions <- sienaNet(totInt[,,firstTime:lastTime])
 
-cluster0 <- sienaNet(clust0[,firstTime:lastTime],type="behavior")
-cluster1 <- sienaNet(clust1[,firstTime:lastTime],type="behavior")
-cluster2 <- sienaNet(clust2[,firstTime:lastTime],type="behavior")
-cluster3 <- sienaNet(clust3[,firstTime:lastTime],type="behavior")
-cluster0 <- makeVarCovar(clust0)
-cluster2 <- makeVarCovar(clust2)
-cluster3 <- makeVarCovar(clust3)
+lowActivityRole <- sienaNet(clust0[,firstTime:lastTime],type="behavior")
+centralRole <- sienaNet(clust1[,firstTime:lastTime],type="behavior")
+periphExpertRole <- sienaNet(clust2[,firstTime:lastTime],type="behavior")
+newbieRole <- sienaNet(clust3[,firstTime:lastTime],type="behavior")
+lowActivityRole <- makeVarCovar(clust0)
+periphExpertRole <- makeVarCovar(clust2)
+newbieRole <- makeVarCovar(clust3)
 
-MyData <- sienaDataCreate(observation, cluster0, cluster1, cluster2, cluster3, logged_edits, activeDays, complex_total,local_talk_total, daysSinceJoining, eigenvectorCentrality)
+MyData <- sienaDataCreate(allInteractions, lowActivityRole, centralRole, periphExpertRole, newbieRole, logged_edits, activeDays, complex_total,local_talk_total, daysSinceJoining, eigenvectorCentrality)
 
 MyEffects <- getEffects(MyData)
 
 print01Report(MyData, MyEffects, modelname="clusterTest")
 
 # Include network effects
-MyEffects <- includeEffects(MyEffects,transTrip,name="observation")
-#MyEffects <- includeEffects(MyEffects,outRate, name="observation",type="rate")
-#MyEffects <- includeEffects(MyEffects, transTriads, balance,name="observation")
+MyEffects <- includeEffects(MyEffects,transTrip, balance,name="allInteractions")
+#MyEffects <- includeEffects(MyEffects,outRate, name="allInteractions",type="rate")
+#MyEffects <- includeEffects(MyEffects, transTriads, balance,name="allInteractions")
 
 # Include Behavior effects
-MyEffects <- includeEffects(MyEffects,simX, interaction1="cluster1", name="observation","creation")
-MyEffects <- includeEffects(MyEffects, name = "cluster1", avAlt, interaction1 = "observation")
+#MyEffects <- includeEffects(MyEffects,simX, interaction1="centralRole", name="allInteractions")
+#MyEffects <- includeEffects(MyEffects, name = "centralRole", avAlt, interaction1 = "allInteractions")
 
 # Covar effects
-#MyEffects <- includeEffects(MyEffects,egoX,simX, interaction1 = "complex_total", name="observation")
-#MyEffects <- includeEffects(MyEffects, RateX, type="rate", interaction1="logged_edits", name="observation")
+#MyEffects <- includeEffects(MyEffects,egoX,simX, interaction1 = "complex_total", name="allInteractions")
+#MyEffects <- includeEffects(MyEffects, RateX, type="rate", interaction1="logged_edits", name="allInteractions")
 
 # Effects on behavior
-#MyEffects <- includeEffects(MyEffects,effFrom, interaction1 = "daysSinceJoining", name="cluster1")
-MyEffects <- includeEffects(MyEffects,effFrom, interaction1 = "eigenvectorCentrality", name="cluster1")
-MyEffects <- includeTimeDummy(MyEffects, density, name="observation", timeDummy = "5")
+#MyEffects <- includeEffects(MyEffects,effFrom, interaction1 = "daysSinceJoining", name="centralRole")
+MyEffects <- includeEffects(MyEffects,effFrom, interaction1 = "eigenvectorCentrality", name="centralRole", type="creation")
+#MyEffects <- includeTimeDummy(MyEffects, density, name="allInteractions", timeDummy = "5")
 
-#MyEffects <- includeEffects(MyEffects, avAltEgoX, name = "cluster1", interaction1="daysSinceJoining", interaction2="localCom")
+#MyEffects <- includeEffects(MyEffects, avAltEgoX, name = "centralRole", interaction1="daysSinceJoining", interaction2="localCom")
 
-#MyEffects <- includeEffects(MyEffects,X,name="observation",interaction1="localCom")
-MyModel <-sienaModelCreate(projname = "observationWithEigCent")
+#MyEffects <- includeEffects(MyEffects,X,name="allInteractions",interaction1="localCom")
+MyModel <-sienaModelCreate(projname = "allInteractionsWithEigCent")
 MyResults <- siena07(MyModel, data=MyData, effects=MyEffects,batch=FALSE)
 
-siena.table(MyResults, type="latex", file="observationEigCentResults.tex", sig=TRUE)
+siena.table(MyResults, type="latex", file="allInteractionsEigCentResults.tex", sig=TRUE)
